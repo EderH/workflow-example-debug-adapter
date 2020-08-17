4dce30bc6c58f7d8f247c422fdb4fa27d575869c
EderH <HansjoergEder@gmx.at>
2020-08-17 15:07:44 +0200

    update to current glsp version

--- src/workflow-runtime.ts
+++ src/workflow-runtime.ts
@@ -13,7 +13,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-import "reflect-metadata";
+// import "reflect-metadata";
 
 import { EventEmitter } from "events";
 import { DebugProtocol } from "vscode-debugprotocol";
